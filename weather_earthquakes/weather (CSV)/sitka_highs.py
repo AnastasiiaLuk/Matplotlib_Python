79db007c135ca85502bd3662c16730a6eab1622b
Anastasia Lukashenko <anastasia.v.lukashenko@gmail.com>
2023-02-21 20:55:17 +0200

    weather

---
+++ weather_earthquakes/weather (CSV)/sitka_highs.py
@@ -0,0 +1,37 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+# filename = '/home/anastasia_luk/Documents/Matplotlib_Python/weather_earthquakes/sitka_weather_07-2018_simple.csv'
+filename = '/weather_earthquakes/weather/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# plt.title('Daily high temperatures, July 2018', fontsize=24)
+plt.title('Daily high and low temperatures - 2018', fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
+
+
